e49e2796904928e8553c3e39a7f6a55e0788fdcf
Yisan Huang <anonymousyisan@gmail.com>
2023-10-16 17:05:30 +0800

    feat: use table

--- frontend/src/components/Point.vue
+++ frontend/src/components/Point.vue
@@ -5,8 +5,6 @@
 
     <el-row>
     <el-col :span="11">
-<!--      <div>{{ sellSide }}</div>-->
-<!--    <div v-for="(item,index) in sellItems" :key="index" class="text item">{{index}} {{ item }}</div>-->
       <el-table :data="buyTableData" style="width: 100%">
         <el-table-column prop="direction" label="方向" />
         <el-table-column prop="ratio" label="比例" />
@@ -18,17 +16,16 @@
         <el-divider direction="vertical" content-position="center" />
       </el-col>
     <el-col :span="11">
-      <div>{{ buySide }}</div>
-    <div v-for="(item,index) in buyItems" :key="index" class="text item">{{index}} {{ item }}</div>
+      <el-table :data="sellTableData" style="width: 100%">
+        <el-table-column prop="direction" label="方向" />
+        <el-table-column prop="ratio" label="比例" />
+        <el-table-column prop="price" label="价格" />
+      </el-table>
     </el-col>
     </el-row>
     <el-divider direction="horizontal" />
     <button @click="printSide">1111</button>
   </el-card>
-
-
-<!--  </el-space>-->
-  <!-- -->
 </template>
 
 <script setup>
@@ -70,9 +67,40 @@ let buyTableData = ref([
     "price": "200"
   },
 ])
+let sellTableData = ref([
+  {
+    "direction": "buy",
+    "ratio": "50x",
+    "price": "1"
+  },
+  {
+    "direction": "buy",
+    "ratio": "50x",
+    "price": "10"
+  },
+  {
+    "direction": "buy",
+    "ratio": "50x",
+    "price": "100"
+  },
+  {
+    "direction": "buy",
+    "ratio": "20x",
+    "price": "2"
+  },
+  {
+    "direction": "buy",
+    "ratio": "20x",
+    "price": "20"
+  },
+  {
+    "direction": "buy",
+    "ratio": "20x",
+    "price": "200"
+  },
+])
 
 function printSide() {
-  let tmpMap = []
   for (let buyItemsKey in buyItems.value) {
     console.log(buyItemsKey)
 
@@ -88,16 +116,14 @@ function printSide() {
   }
 }
 
-function makeTable(tableData,items,direction) {
+const makeTable = (tableData, direction, items) => {
   tableData.value = []
-  for (let valueKey in items.value) {
-    console.log(tableData.value[valueKey].length)
-    for (let i = 0; i < tableData.value[valueKey].length; i++) {
-
-      buyTableData.value.push({
+  for (let itemsKey in items.value) {
+    for (let i = 0; i < items.value[itemsKey].length; i++) {
+      tableData.value.push({
         "direction": direction,
-        "ratio": valueKey,
-        "price": tableData.value[valueKey][i]
+        "ratio": itemsKey,
+        "price": items.value[itemsKey][i]
       })
     }
   }
@@ -106,23 +132,13 @@ function makeTable(tableData,items,direction) {
 watch(() => props.res.buyData, (newVal, oldVal) => {
   buySide.value = newVal.side
   buyItems.value = newVal.data
-  buyTableData.value = []
-  for (let buyItemsKey in buyItems.value) {
-    for (let i = 0; i < buyItems.value[buyItemsKey].length; i++) {
-
-      buyTableData.value.push({
-        "direction": "buy",
-        "ratio": buyItemsKey,
-        "price": buyItems.value[buyItemsKey][i]
-      })
-    }
-  }
+  makeTable(buyTableData, "buy", buyItems)
 })
 
 watch(() => props.res.sellData, (newVal, oldVal) => {
-  console.log(`new: ${newVal.side}`)
   sellSide.value = newVal.side
   sellItems.value = newVal.data
+  makeTable(sellTableData, "sell", sellItems)
 })
 
 
